8c1acf7c9a5adf15fc7c4a5dc887f83c1e6d09ee
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-07-02 21:13:23 +0330

    Add django-filter lib for search product

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'accounts',
     'rest_framework_simplejwt',
@@ -65,39 +66,29 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': "railway",
-#         'USER': "postgres",
-#         'PASSWORD': "bCqAEcFXjMRupUISSduByrwKmgXRxlAg",
-#         'HOST': os.getenv('DB_HOST', 'postgres.railway.internal'),
-#         'PORT': os.getenv('DB_PORT', '5432'),
-#     }
-# }
-
-
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "railway",
+        'USER': "postgres",
+        'PASSWORD': "bCqAEcFXjMRupUISSduByrwKmgXRxlAg",
+        'HOST': os.getenv('DB_HOST', 'postgres.railway.internal'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST", "localhost"),
+#         "PORT": os.getenv("DB_PORT", "5432"),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
