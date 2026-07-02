5ac00a3e3db378a52df8c17170d74697c865e2fb
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-07-02 21:39:46 +0330

    fix bug

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_filters',
     'rest_framework',
     'accounts',
     'rest_framework_simplejwt',
