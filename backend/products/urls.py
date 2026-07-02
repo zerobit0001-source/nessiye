92ecb48e3865eec2d51e242abd0b625d29b7bc0c
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-07-02 15:34:17 +0330

    Create model url

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductListCreateView, ProductDetailView, CategoryListCreateView
+from .views import ProductListCreateView, ProductDetailView, CategoryListCreateView, ModalView
 
 urlpatterns = [
     path('products/', ProductListCreateView.as_view(), name='product-list-create'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
     path('categories/', CategoryListCreateView.as_view(), name='category-list-create'),
+    path('modal/', ModalView.as_view()),
 ]
