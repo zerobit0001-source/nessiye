d2adcab483d4cbaf1d7648ba8ca846bf97e6f893
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-07-02 15:27:11 +0330

    Create payment list url

--- backend/debts/urls.py
+++ backend/debts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import DebtListView, DebtDetailView, DebtPayView
+from .views import DebtListView, DebtDetailView, DebtPayView, PaymentListView
 
 urlpatterns = [
     path('debts/', DebtListView.as_view()),
     path('debts/<int:pk>/', DebtDetailView.as_view()),
     path('debts/<int:pk>/pay/', DebtPayView.as_view()),
+    path('payments/', PaymentListView.as_view()),
 ]
