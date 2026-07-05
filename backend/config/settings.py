9c1113caa3d19bc09e3e74f87b452539f748a36b
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-07-05 07:46:15 +0330

    add silk app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'silk',
     'accounts',
     'rest_framework_simplejwt',
     'products',
