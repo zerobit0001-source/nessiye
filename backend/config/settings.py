146ad9465c134702cfd82d5f9d3d6950f3b797a0
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-07-05 07:56:38 +0330

    fix bug

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'silk.middleware.SilkyMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
