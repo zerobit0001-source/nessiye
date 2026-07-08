fd09d8005b4ca88533ff3390f6d7bde18ed78940
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-07-08 10:21:48 +0330

    Create dashboard endpoint

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProductListCreateView, ProductDetailView, CategoryListCreateView, ModalView
+from .views import ProductListCreateView, ProductDetailView, CategoryListCreateView, ModalView, DashboardView
 
 urlpatterns = [
     path('products/', ProductListCreateView.as_view(), name='product-list-create'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
     path('categories/', CategoryListCreateView.as_view(), name='category-list-create'),
     path('modal/', ModalView.as_view()),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
