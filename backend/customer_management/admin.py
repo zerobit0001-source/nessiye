1385a89d513c9e7add73aa9fa366d8e183988f12
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-07-08 10:36:23 +0330

    Create admin for customer_management

--- backend/customer_management/admin.py
+++ backend/customer_management/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CustomerShop
 
-# Register your models here.
+
+@admin.register(CustomerShop)
+class CustomerShopAdmin(admin.ModelAdmin):
+    list_display = ['id', 'shop', 'customer', 'created_at']
+    list_filter = ['shop']
+    search_fields = ['shop__shop_name', 'customer__phone_number', 'customer__full_name']
+    readonly_fields = ['created_at']
