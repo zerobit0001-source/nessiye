efb94e04377bf2952f7407c33a31bc67b968cd04
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-07-15 19:24:36 +0330

    Create overdue debts url

--- backend/debts/urls.py
+++ backend/debts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import DebtListView, DebtDetailView, DebtPayView, PaymentListView, CustomerDebtPayView
+from .views import DebtListView, DebtDetailView, DebtPayView, OverdueDebtsView, PaymentListView, CustomerDebtPayView
 
 urlpatterns = [
     path('debts/', DebtListView.as_view()),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('debts/<int:pk>/pay/', DebtPayView.as_view()),
     path('debts/<str:debt_id>/customer_pay/', CustomerDebtPayView.as_view()),
     path('payments/', PaymentListView.as_view()),
+    path('overdue/', OverdueDebtsView.as_view()),
 ]
