ebad585267e0936c26ae78db6d3e7dae39e58a4e
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-07-16 17:23:31 +0330

    Create model for activities

--- backend/activity/models.py
+++ backend/activity/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Activity(models.Model):
+    shop = models.ForeignKey(User, on_delete=models.CASCADE)
+    action = models.CharField(max_length=20)
+    entity = models.CharField(max_length=20)
+    object_id = models.PositiveIntegerField(null=True, blank=True)
+    metadata = models.JSONField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Activities'
+        ordering = ['-created_at']
