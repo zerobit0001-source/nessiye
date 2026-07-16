fa866c7c814cf3b7f15f5d2eb61d271c5a599bbb
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-07-16 17:26:15 +0330

    Create model for activities

--- backend/activity/models.py
+++ backend/activity/models.py
@@ -5,6 +5,7 @@ class Activity(models.Model):
     shop = models.ForeignKey(User, on_delete=models.CASCADE)
     action = models.CharField(max_length=20)
     entity = models.CharField(max_length=20)
+    title = models.CharField(max_length=255)
     object_id = models.PositiveIntegerField(null=True, blank=True)
     metadata = models.JSONField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
