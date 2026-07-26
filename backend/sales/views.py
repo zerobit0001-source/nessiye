1817fada4e839cc50f421d1b3279055c0dfad967
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-07-26 20:52:22 +0330

    fix timedelta datetime pk

--- backend/sales/views.py
+++ backend/sales/views.py
@@ -11,7 +11,7 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from datetime import timedelta
 from .models import Sale, SaleItem
 from .serializers import SaleItemSerializer, SaleSerializer
 
