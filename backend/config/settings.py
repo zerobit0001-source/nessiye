1b6cb20e9fdd9fd810a17c6a026b6f622a98ba58
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-08-02 11:05:34 +0330

    config cache

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -98,6 +98,16 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/1'),
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
