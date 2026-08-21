73659b41faefdf5ab1ad745a7268efddcf406c3e
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-08-21 11:39:11 +0330

    Update project config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -8,12 +8,12 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = "django-insecure-9o*=u$w&n-$)w^ga($k#&&g_&23$!h_)6g#hp@3v_em2z0za#i"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 
-# DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-DEBUG = True
+# DEBUG = True
 
 
 ALLOWED_HOSTS = ["*"]
