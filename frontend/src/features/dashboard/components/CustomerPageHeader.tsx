caaa2b531e729d8349ee312458eae45c10dba12a
amirrezaabdolahi <amirenzevadev@gmail.com>
2026-08-21 12:22:03 +0330

    frontend - udpated the link of create debt payment sale , customer detail page

--- frontend/src/features/dashboard/components/CustomerPageHeader.tsx
+++ frontend/src/features/dashboard/components/CustomerPageHeader.tsx
@@ -4,25 +4,40 @@ import Link from "next/link";
 import React from "react";
 import AddPaymentModal from "./AddPaymentModal";
 import AddSaleModal from "./AddSaleModal";
+import LinkButton from "./LinkButton";
 
-const CustomerPageHeader = () => {
-    return (
-        <div className="w-full flex items-center justify-between">
-            <Link href={"/dashboard/customers"}>
-                <Button
-                    size="small"
-                    startIcon={<ArrowForwardIosRounded fontSize="small" />}
-                    variant="outlined"
-                >
-                    برگشت
-                </Button>
-            </Link>
-            <Box className="flex gap-2">
-                <AddSaleModal />
-                <AddPaymentModal />
-            </Box>
-        </div>
-    );
+const CustomerPageHeader = ({ id }: { id: string }) => {
+  return (
+    <div className="w-full flex items-center justify-between">
+      <Link href={"/dashboard/customers"}>
+        <Button
+          size="small"
+          startIcon={<ArrowForwardIosRounded fontSize="small" />}
+          variant="outlined"
+        >
+          برگشت
+        </Button>
+      </Link>
+      <Box className="flex gap-2">
+        {/*<AddSaleModal />*/}
+        <LinkButton
+          text="بدهی"
+          link={`/dashboard/debts/create?customerId=${id}`}
+          variant="outlined"
+        />
+        <LinkButton
+          text="فروش"
+          link={`/dashboard/sales/create?customerId=${id}`}
+          variant="outlined"
+        />
+        <LinkButton
+          text="پرداخت"
+          link={`/dashboard/payments/create?customerId=${id}`}
+          variant="contained"
+        />
+      </Box>
+    </div>
+  );
 };
 
 export default CustomerPageHeader;
